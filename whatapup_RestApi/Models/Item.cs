c8ab2b3e80d69400f6d09f664c8ffc42741633f7
arvimanninen <arvi.manninen@zoho.eu>
2018-09-21 18:01:55 +0300

    whatapup_RestApi project created. Category and Item models created.

---
+++ whatapup_RestApi/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace whatapup_RestApi.Models
+{
+    public class Item
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public bool Completed { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
