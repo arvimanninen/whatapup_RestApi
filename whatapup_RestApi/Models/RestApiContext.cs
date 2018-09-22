fe298c4c6018401f1164ecc2ae5948262f75f712
arvimanninen <arvi.manninen@zoho.eu>
2018-09-22 16:56:30 +0300

    Necessary code added to RestApiContext. Entity Framework Core dependency added. RestApiContext registered with dependency injection (Startup.cs).

--- whatapup_RestApi/Models/RestApiContext.cs
+++ whatapup_RestApi/Models/RestApiContext.cs
@@ -8,6 +8,11 @@ namespace whatapup_RestApi.Models
 {
     public class RestApiContext : DbContext
     {
-        public RestApiContext()
+        public RestApiContext(DbContextOptions<RestApiContext> options) 
+            : base(options)
+        { }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
+
